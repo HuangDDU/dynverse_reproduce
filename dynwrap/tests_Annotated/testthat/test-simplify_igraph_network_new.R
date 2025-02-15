91f2c2193a5010a10bd1d1562d78f997007902c3
黄照样 <554598474@qq.com>
2025-02-15 17:33:37 +0800

    simplify_trajector undirected_graph testcase

--- dynwrap/tests_Annotated/testthat/test-simplify_igraph_network_new.R
+++ dynwrap/tests_Annotated/testthat/test-simplify_igraph_network_new.R
@@ -5,45 +5,68 @@ library(tidyverse)
 
 context("Testing simplify_igraph_network")
 
-test_that("directed_linear", {
-  directed <- TRUE
-  net <- tribble(
-    ~from, ~to,
-    "A", "B",
-    "B", "C",
-    "C", "D",
-  )
-  gr <- igraph::graph_from_data_frame(net, directed = directed)
-  edge_points <- tribble(
-    ~id, ~from, ~to, ~percentage,
-    "a", "A", "B", 0.3,
-    "b", "A", "B", 0.6,
-    "c", "B", "C", 0.2,
-    "d", "B", "C", 0.8,
-    "e", "C", "D", 0.4,
-  )
+# input data
+directed <- TRUE
+net <- tribble(
+  ~from, ~to,
+  "A", "B",
+  "B", "C",
+  "C", "D",
+)
+gr <- igraph::graph_from_data_frame(net, directed = directed)
+edge_points <- tribble(
+  ~id, ~from, ~to, ~percentage,
+  "a", "A", "B", 0.3,
+  "b", "A", "B", 0.6,
+  "c", "B", "C", 0.2,
+  "d", "B", "C", 0.8,
+  "e", "C", "D", 0.4,
+)
 
-  # 简化milestone network的同时，需要简化网络上边的细胞节点
+# expected result
+expected_net <- tribble(
+  ~from, ~to, ~weight, ~directed,
+  "A", "D", 3, TRUE
+)
+expected_edge_points <- tribble(
+  ~id, ~from, ~to, ~percentage,
+  "a", "A", "D", 0.1,
+  "b", "A", "D", 0.2,
+  "c", "A", "D", 0.4,
+  "d", "A", "D", 0.6,
+  "e", "A", "D", 0.8,
+)
+
+test_that("directed linear", {
+  # execute function
   out <- simplify_igraph_network(gr, allow_duplicated_edges = FALSE, edge_points = edge_points)
   new_gr <- out$gr
   new_edge_points <- out$edge_points
-
-  # 预期输出
-  expected_net <- tribble(
-    ~from, ~to, ~weight, ~directed,
-    "A", "D", 3, TRUE
-  )
-  expected_edge_points <- tribble(
-    ~id, ~from, ~to, ~percentage,
-    "a", "A", "D", 0.1,
-    "b", "A", "D", 0.2,
-    "c", "A", "D", 0.4,
-    "d", "A", "D", 0.6,
-    "e", "A", "D", 0.8,
-  )
-
   new_net <- tibble(igraph::as_data_frame(new_gr))
-  testthat::expect_equal(new_net, expected_net)
   new_edge_points <- tibble(new_edge_points[, c("id", "from", "to", "percentage")])
+
+  testthat::expect_equal(new_net, expected_net)
+  testthat::expect_equal(new_edge_points, expected_edge_points)
+})
+
+
+test_that("undirected linear", {
+  # input data
+  gr <- igraph::graph_from_data_frame(net, directed = FALSE)
+  expected_net["directed"] <-  FALSE
+
+  # expected result
+  expected_milestone_network <- tribble(
+    ~from, ~to, ~length, ~directed,
+    "A", "D", 3, FALSE
+  )
+  # execute function
+  out <- simplify_igraph_network(gr, allow_duplicated_edges = FALSE, edge_points = edge_points)
+  new_gr <- out$gr
+  new_edge_points <- out$edge_points
+  new_net <- tibble(igraph::as_data_frame(new_gr))
+  new_edge_points <- tibble(new_edge_points[, c("id", "from", "to", "percentage")])
+
+  testthat::expect_equal(new_net, expected_net)
   testthat::expect_equal(new_edge_points, expected_edge_points)
 })
--- dynwrap/tests_Annotated/testthat/test-simplify_trajectory_new.R
+++ dynwrap/tests_Annotated/testthat/test-simplify_trajectory_new.R
@@ -6,24 +6,61 @@ library(tidyverse)
 
 context("Testing simplify_trajectory_new")
 
-test_that("directed_linear", {
-  id <- "directed_linear"
-  cell_ids <- c("a", "b", "c", "d", "e")
-  milestone_ids <- c("A", "B", "C", "D")
-  milestone_network <- tribble(
-    ~from, ~to, ~length, ~directed,
-    "A", "B", 1, TRUE,
-    "B", "C", 1, TRUE,
-    "C", "D", 1, TRUE,
-  )
-  progressions <- tribble(
-    ~cell_id, ~from, ~to, ~percentage,
-    "a", "A", "B", 0.3,
-    "b", "A", "B", 0.6,
-    "c", "B", "C", 0.2,
-    "d", "B", "C", 0.8,
-    "e", "C", "D", 0.4,
-  )
+# input data
+id <- "directed linear"
+cell_ids <- c("a", "b", "c", "d", "e")
+milestone_ids <- c("A", "B", "C", "D")
+milestone_network <- tribble(
+  ~from, ~to, ~length, ~directed,
+  "A", "B", 1, TRUE,
+  "B", "C", 1, TRUE,
+  "C", "D", 1, TRUE,
+)
+progressions <- tribble(
+  ~cell_id, ~from, ~to, ~percentage,
+  "a", "A", "B", 0.3,
+  "b", "A", "B", 0.6,
+  "c", "B", "C", 0.2,
+  "d", "B", "C", 0.8,
+  "e", "C", "D", 0.4,
+)
+trajectory <-
+  wrap_data(
+    id = id,
+    cell_ids = cell_ids
+  ) %>%
+    add_trajectory(
+      milestone_ids = milestone_ids,
+      milestone_network = milestone_network,
+      progressions = progressions
+    )
+
+# expected result
+expected_milestone_network <- tribble(
+  ~from, ~to, ~length, ~directed,
+  "A", "D", 3, TRUE
+)
+expected_progressions <- tribble(
+  ~cell_id, ~from, ~to, ~percentage,
+  "a", "A", "D", 0.1,
+  "b", "A", "D", 0.2,
+  "c", "A", "D", 0.4,
+  "d", "A", "D", 0.6,
+  "e", "A", "D", 0.8,
+)
+
+test_that("directed linear", {
+  # execute function
+  simp <- simplify_trajectory(trajectory)
+
+  testthat::expect_equal(simp$milestone_network, as.data.frame(expected_milestone_network))
+  testthat::expect_equal(simp$progression, as.data.frame(expected_progressions))
+})
+
+
+test_that("undirected linear", {
+  # input data
+  milestone_network["directed"] <- FALSE # 设置为无向图
   trajectory <-
     wrap_data(
       id = id,
@@ -34,22 +71,16 @@ test_that("directed_linear", {
         milestone_network = milestone_network,
         progressions = progressions
       )
-  simp <- simplify_trajectory(trajectory)
 
-  # 预期输出
+  # expected result
   expected_milestone_network <- tribble(
     ~from, ~to, ~length, ~directed,
-    "A", "D", 3, TRUE
+    "A", "D", 3, FALSE
   )
-  expected_progressions <- tribble(
-    ~cell_id, ~from, ~to, ~percentage,
-    "a", "A", "D", 0.1,
-    "b", "A", "D", 0.2,
-    "c", "A", "D", 0.4,
-    "d", "A", "D", 0.6,
-    "e", "A", "D", 0.8,
-  )
+  # execute function
+  simp <- simplify_trajectory(trajectory)
 
   testthat::expect_equal(simp$milestone_network, as.data.frame(expected_milestone_network))
   testthat::expect_equal(simp$progression, as.data.frame(expected_progressions))
 })
+milestone_network
